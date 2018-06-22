796dab06754d1edb524e4aa324d0cff3c6660470
egor21312 <anton255260@gmail.com>
2018-06-22 16:05:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,68 +6,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const recipepuppyHost = 'http://www.recipepuppy.com/api/?q=';
-const currencyConvertHost = "http://api.fixer.io/latest?";
-const chucknorrisHost = 'https://api.chucknorris.io/jokes/random';
 const wikiPediaApiHost = 'https://ru.wikipedia.org/w/api.php?'; //https://www.mediawiki.org/wiki/API:Opensearch
 
-app.get('/dummyget', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ 'speech': 'dummy speech', 'displayText': 'dummy get works!' }));
-});
-
-
 app.post('/webhook', function (req, res) {
-
-    if (req.body.result.parameters['Bored']) {
-        callChuckNorrisFact()
-            .then((output) => {
-                let result = toApiAiResponseMessage(output.value, output.value, toTelgramObject(output.value, 'Markdown'));
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result));
-            })
-            .catch(errorHandler);
-    }
-    else if (req.body.result.parameters['FoodItem']) {
-        var fooditem = req.body.result.parameters['FoodItem'];
-        callRecipePuppy(fooditem)
-            .then((output) => {
-
-                let displayText = `Found recipe for: ${output.title} at ${output.href}`;
-                let telegramText = htmlEntities('*Found*-' + output.title + '\n' + '* It has following Ingredients*-' + output.ingredients + '\n' + '* You can check it out at*- ' + output.href);
-                let result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(telegramText, 'Markdown'));
-                console.log(result);
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result));
-            })
-            .catch(errorHandler);
-    }
-    else if (req.body.result.parameters['currency-from'] && req.body.result.parameters['currency-to']) {
-        var currencyFrom = req.body.result.parameters['currency-from'];
-        var currencyTo = req.body.result.parameters['currency-to'];
-        var number = 1.0;
-        if (req.body.result.parameters['number']) {
-            number = parseFloat(req.body.result.parameters['number']);
-            if (number <= 0) {
-                number = 1.0;
-            }
-        }
-        callFixerIo(currencyFrom, currencyTo)
-            .then((output) => {
-                let resultText = Array();
-                currencyTo.forEach(function (cur) {
-                    var toNumber = number * parseFloat(output.rates[cur.toUpperCase()]);
-                    toNumber = toNumber.toFixed(3);
-                    resultText.push(`${number} ${output.base} = ${toNumber} ${cur}`);
-                }, this);
-
-                let displayText = resultText.join();
-                let result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(resultText.join('\n'), 'Markdown'));
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result));
-            });
-    }
-    else if (req.body.result.parameters['wikisearchterm']) {
+    if (req.body.result.parameters['wikisearchterm']) {
         var searchTerm = req.body.result.parameters['wikisearchterm'];
         callWikiPediaApi(searchTerm)
             .then((output) => {
@@ -75,7 +17,7 @@ app.post('/webhook', function (req, res) {
                 let result;
                 if (output && output[0]) {
                     displayText = `Вот что я нашёл в Википедии по запросу ${output[1][0]}: ${output[2][0]}`;
-                    let telegramText = htmlEntities(`Вот что я нашёл в Википедии по запросу *${output[1][0]}*: ${output[2][0]} \n\n Read more at [WikiPedia](${output[3][0]})`);
+                    let telegramText = htmlEntities(`Вот что я нашёл в Википедии по запросу *${output[1][0]}*: ${output[2][0]} \n\n Прочитать полностью в [Wikipedia](${output[3][0]})`);
                     result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(telegramText, 'Markdown'));
                 }
                 res.setHeader('Content-Type', 'application/json');
@@ -92,61 +34,6 @@ app.post('/webhook', function (req, res) {
         res.send(JSON.stringify({ 'speech': "No Proper hook found", 'displayText': "No Proper hook found" }));
     }
 });
-
-
-function callRecipePuppy(fooditem) {
-    return new Promise((resolve, reject) => {
-        http.get(recipepuppyHost + fooditem, (res) => {
-            let body = '';
-            res.on('data', (d) => body += d);
-            res.on('end', () => {
-                let jO = JSON.parse(body);
-                let firstItem = jO.results[Math.floor((Math.random() * jO.results.length))];
-                resolve(firstItem);
-            });
-
-            res.on('error', (error) => {
-                reject(error);
-            });
-        });
-    });
-}
-
-function callFixerIo(currencyFrom, currencyTo) {
-    return new Promise((resolve, reject) => {
-        currencyTo = currencyTo.join().toUpperCase();
-        let url = `${currencyConvertHost}base=${currencyFrom}&symbols=${currencyTo}`;
-        http.get(url, (res) => {
-            let body = '';
-            res.on('data', (d) => body += d);
-            res.on('end', () => {
-                let jO = JSON.parse(body);
-                resolve(jO);
-            });
-
-            res.on('error', (error) => {
-                reject(error);
-            });
-        });
-    });
-}
-
-function callChuckNorrisFact() {
-    return new Promise((resolve, reject) => {
-        https.get(chucknorrisHost, (res) => {
-            let body = '';
-            res.on('data', (d) => body += d);
-            res.on('end', () => {
-                let jO = JSON.parse(body);
-                resolve(jO);
-            });
-
-            res.on('error', (error) => {
-                reject(error);
-            });
-        });
-    });
-}
 
 function callWikiPediaApi(searchTerm, format = "json", action = "opensearch", limit = 2, profile = "fuzzy") {
     return new Promise((resolve, reject) => {
